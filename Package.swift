dcc0d36a85a6a20012e304ba0ade67c17a661a39
Curtis Elswick <curtis.elswick@artiusid.dev>
2025-08-21 10:13:29 -0600

    Release v1.0.127

--- Package.swift
+++ Package.swift
@@ -14,13 +14,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "OpenSSL",
-            url: "https://github.com/artiusID/sdk/releases/download/v1.0.123/OpenSSL.xcframework.zip",
-            checksum: "d2a817cd6b773c3f28dc5d2cef21b670344841365a364d4b16d11a8734aa49ab"
+            url: "https://github.com/artiusID/sdk/releases/download/v1.0.127/OpenSSL.xcframework.zip",
+            checksum: "6e8212ae546c276f838bebe4b8f20edbb9da960cce9a2f481974ea22b72ae4fb"
         ),
         .binaryTarget(
             name: "artiusid_sdk_ios",
-            url: "https://github.com/artiusID/sdk/releases/download/v1.0.123/artiusid_sdk_ios.xcframework.zip",
-            checksum: "7a298d79cc9236eb42e67a3a8066296eadbd6523d20f76e4d5a3127145ddfe10"
+            url: "https://github.com/artiusID/sdk/releases/download/v1.0.127/artiusid_sdk_ios.xcframework.zip",
+            checksum: "efa17834d4c92bff741e3181d0268e03d5a9f654171256d69463b61b693d6647"
         ),
         .target(
             name: "ArtiusIDSDKWrapper",
