1b9705cf1cfa5e664676bb54324b895713b1855d
Curtis Elswick <curtis.elswick@artiusid.dev>
2025-09-03 11:19:37 -0600

    Release v1.0.175

--- Package.swift
+++ Package.swift
@@ -14,12 +14,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "OpenSSL",
-            url: "https://github.com/artiusID/sdk/releases/download/v1.0.174/OpenSSL.xcframework.zip",
+            url: "https://github.com/artiusID/sdk/releases/download/v1.0.175/OpenSSL.xcframework.zip",
             checksum: "7ac5f0580bf367d75adbe198e0cab2f4b230c9694ef8ca27052bd8be36c8962d"
         ),
         .binaryTarget(
             name: "artiusid_sdk_ios",
-            url: "https://github.com/artiusID/sdk/releases/download/v1.0.174/artiusid_sdk_ios.xcframework.zip",
+            url: "https://github.com/artiusID/sdk/releases/download/v1.0.175/artiusid_sdk_ios.xcframework.zip",
             checksum: "ce56405cd600930fae6813f39d2e5ed84882040d10e3fd98ce91d4bbe07150ca"
         ),
         .target(
@@ -31,7 +31,8 @@ let package = Package(
             path: "Sources",
             sources: [
                 "ArtiusIDSDKWrapper.swift",
-                "VerificationResult.swift"
+                "VerificationResult.swift",
+                "LogManager.swift"
             ]
         )
     ]
