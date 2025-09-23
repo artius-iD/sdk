6f35ac5b758a0408c075cce7f01908d41c99857d
artiusID <curtis.elswick@artiusid.dev>
2025-09-23 14:08:49 -0600

    temp rollback

--- Package.swift
+++ Package.swift
@@ -14,12 +14,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "OpenSSL",
-            url: "https://github.com/artiusID/sdk/releases/download/v1.0.210/OpenSSL.xcframework.zip",
+            url: "https://github.com/artiusID/sdk/releases/download/v1.0.207/OpenSSL.xcframework.zip",
             checksum: "7a8dd82295acdd4256c29df25d915bf5c92f22b202a5eef1c0bc7247b5aac361"
         ),
         .binaryTarget(
             name: "artiusid_sdk_ios",
-            url: "https://github.com/artiusID/sdk/releases/download/v1.0.210/artiusid_sdk_ios.xcframework.zip",
+            url: "https://github.com/artiusID/sdk/releases/download/v1.0.207/artiusid_sdk_ios.xcframework.zip",
             checksum: "4b5abe26996e8ec2dc0401674526bd0016bcb89849b0f834473897916eff2139"
         ),
         .target(
