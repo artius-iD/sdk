189a8fd5d34e30fa265e347ae91431a7e65be7cc
Nelson Soler <nsoler@frontier.com>
2025-09-30 09:00:20 -0700

    Release v1.0.229

--- Package.swift
+++ Package.swift
@@ -14,13 +14,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "OpenSSL",
-            url: "https://github.com/artius-iD/sdk/releases/download/v1.0.228/OpenSSL.xcframework.zip",
-            checksum: "f16fe1d210044b493002619c597ecf159b4024641c88b78d33ed4f26c838a4dc"
+            url: "https://github.com/artius-iD/sdk/releases/download/v1.0.229/OpenSSL.xcframework.zip",
+            checksum: "15d2b98ae8d7bf4fc783fd32937b621408cdeeaeeb4a010acf5110cf5c111787"
         ),
         .binaryTarget(
             name: "artiusid_sdk_ios",
-            url: "https://github.com/artius-iD/sdk/releases/download/v1.0.228/artiusid_sdk_ios.xcframework.zip",
-            checksum: "ea4629ec82b39eb5830d4255677518821685b3a235482356f17fa440befba20a"
+            url: "https://github.com/artius-iD/sdk/releases/download/v1.0.229/artiusid_sdk_ios.xcframework.zip",
+            checksum: "cb47462d24e497248e87e07d110036cdf343336c1b366f84b69657848d1efb6d"
         ),
         .target(
             name: "ArtiusIDSDKWrapper",
