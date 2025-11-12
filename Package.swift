5f525ce1c34095394cc51313834af934fd9a5024
Todd Bryant <todd.bryant@artiusid.dev>
2025-11-12 11:57:43 -0600

    Release v2.0.3
    - ArtiusID iOS SDK v2.0.3
    - Client integration documentation
    - Version history (CHANGELOG.md)
    - Theme customization
    - FCM notification support
    - Biometric authentication

--- Package.swift
+++ Package.swift
@@ -14,13 +14,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "OpenSSL",
-            url: "https://github.com/artius-iD/sdk/releases/download/v2.0.2/OpenSSL.xcframework.zip",
-            checksum: "587e2573abec3ae4d02e0701245655a72563fb266c9bffbd6d15bae8ae401280"
+            url: "https://github.com/artius-iD/sdk/releases/download/v2.0.3/OpenSSL.xcframework.zip",
+            checksum: "9efe3bd10b0945c9b5763b253b3d9d510a6e3712cd26c7b6509409ecea492b96"
         ),
         .binaryTarget(
             name: "artiusid_sdk_ios",
-            url: "https://github.com/artius-iD/sdk/releases/download/v2.0.2/artiusid_sdk_ios.xcframework.zip",
-            checksum: "61ed6e856d74b6108791890dd5fc742f2dc53d27446ab6e5ce86167ca2091f1c"
+            url: "https://github.com/artius-iD/sdk/releases/download/v2.0.3/artiusid_sdk_ios.xcframework.zip",
+            checksum: "2fdd0cb8eae12bf2b893363f556bdb2fcf300fc05b54785aecd7efe7bb871c97"
         ),
         .target(
             name: "ArtiusIDSDKWrapper",
